f2923743aed928f95ef1a882c3ec01cc583f91fe
pranay <pranayetta.1@gmail.com>
2024-02-27 22:54:23 -0700

    RoboShop Shell Script

--- Frontend.sh
+++ Frontend.sh
@@ -1,8 +1,15 @@
+echo -e "\e[31mInstalling the Nginx Server\e[0m"
 dnf install nginx -y
+
+echo -e "\e[32mRemoving  the previous Content\e[0m"
 rm -rf /usr/share/nginx/html/*
+
+echo -e "\e[33mDownloading the Frontend Content\e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 #We need to create a file here
+
+echo -e "\e[34mRestarting the Nginx Server\e[0m"
 systemctl enable nginx
 systemctl restart nginx
