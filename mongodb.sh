2504362458f5855dc39186222bbc50b4887fb88f
pranay <pranayetta.1@gmail.com>
2024-03-01 10:43:28 -0700

    Mongodb script

--- mongodb.sh
+++ mongodb.sh
@@ -7,6 +7,9 @@ echo -e "\e[36mInstalling the mongodb\e[0m"
 dnf install mongodb-org -y
 
 #modify the file
+echo -e "\e[32mModifying the file\e[0m"
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+
 echo -e "\e[33mRestarting the mongodb\e[0m"
 systemctl enable mongod
 systemctl restart mongod
