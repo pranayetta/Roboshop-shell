97335b31dc2223f303bb9f8367d4bb3579518e03
pranay <pranayetta.1@gmail.com>
2024-03-01 11:38:28 -0700

    Roboshop shell script

--- Frontend.sh
+++ Frontend.sh
@@ -5,11 +5,12 @@ echo -e "\e[32mRemoving  the previous Content\e[0m"
 rm -rf /usr/share/nginx/html/* &>>/tmp/roboshop.log
 
 echo -e "\e[33mDownloading the Frontend Content\e[0m"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>/tmp/roboshop.log
 cd /usr/share/nginx/html &>>/tmp/roboshop.log
 unzip /tmp/frontend.zip &>>/tmp/roboshop.log
 
 #We need to create a file here
+echo -e "\e[36mCopying the file\e[0m"
 cp /home/centos/Roboshop-shell/roboshop.conf /etc/nginx/default.d/roboshop.conf &>>/tmp/roboshop.log
 
 echo -e "\e[34mRestarting the Nginx Server\e[0m"
