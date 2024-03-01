cc6deaedd086c0d1a3e1791a773b25b2d6c67ec7
pranay <pranayetta.1@gmail.com>
2024-03-01 11:29:01 -0700

    Roboshop shell script

--- Frontend.sh
+++ Frontend.sh
@@ -8,7 +8,9 @@ echo -e "\e[33mDownloading the Frontend Content\e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 cd /usr/share/nginx/html &>>/tmp/roboshop.log
 unzip /tmp/frontend.zip &>>/tmp/roboshop.log
+
 #We need to create a file here
+cp /home/centos/Roboshop-shell/roboshop.conf /etc/nginx/default.d/roboshop.conf &>>/tmp/roboshop.log
 
 echo -e "\e[34mRestarting the Nginx Server\e[0m"
 systemctl enable nginx &>>/tmp/roboshop.log
