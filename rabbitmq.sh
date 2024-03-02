3778e9ec71b910442a0460368740ec5bb6069685
pranay <pranayetta.1@gmail.com>
2024-03-01 17:12:18 -0700

    Roboshop shell script

---
+++ rabbitmq.sh
@@ -0,0 +1,16 @@
+echo -e "\e[32mYum repos for erlang\e[0m"
+curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash &>>/tmp/roboshop.log
+
+echo -e "\e[32mYum repos for rabbitmq\e[0m"
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash &>>/tmp/roboshop.log
+
+echo -e "\e[32mInstall Rabbitmq\e[0m"
+dnf install rabbitmq-server -y &>>/tmp/roboshop.log
+
+echo -e "\e[32mEnable and restart\e[0m"
+systemctl enable rabbitmq-server &>>/tmp/roboshop.log
+systemctl restart rabbitmq-server &>>/tmp/roboshop.log
+
+echo -e "\e[32mAdding user and password for rabbitmq\e[0m"
+rabbitmqctl add_user roboshop roboshop123 &>>/tmp/roboshop.log
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>/tmp/roboshop.log
