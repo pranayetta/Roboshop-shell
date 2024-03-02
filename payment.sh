50109a4c121f57a2f3ba2e12382c9093ca2a1400
pranay <pranayetta.1@gmail.com>
2024-03-01 17:25:08 -0700

    Roboshop shell script

---
+++ payment.sh
@@ -0,0 +1,29 @@
+echo -e "\e[32mInstalling python\e[0m"
+dnf install python36 gcc python3-devel -y
+
+echo -e "\e[32mAdding the application user\e[0m"
+useradd roboshop &>>/tmp/roboshop.log
+
+echo -e "\e[32mCreating the application directory\e[0m"
+rm -rf /app &>>/tmp/roboshop.log
+mkdir /app &>>/tmp/roboshop.log
+
+echo -e "\e[32mDownloading the application code and extracting\e[0m"
+curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment.zip &>>/tmp/roboshop.log
+cd /app &>>/tmp/roboshop.log
+unzip /tmp/payment.zip &>>/tmp/roboshop.log
+
+echo -e "\e[32mInstalling the dependencies\e[0m"
+cd /app &>>/tmp/roboshop.log
+pip3.6 install -r requirements.txt &>>/tmp/roboshop.log
+
+echo -e "\e[32mCopying the payment service file\e[0m"
+cp /home/centos/Roboshop-shell/payment.service /etc/systemd/system/payment.service &>>/tmp/roboshop.log
+
+echo -e "\e[32mReloading\e[0m"
+systemctl daemon-reload &>>/tmp/roboshop.log
+
+echo -e "\e[32mEnable and restart\e[0m"
+systemctl enable payment &>>/tmp/roboshop.log
+systemctl restart payment &>>/tmp/roboshop.log
+
