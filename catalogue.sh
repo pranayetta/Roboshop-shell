8d21c417dcf689ba1b87ac24e273852788262193
pranay <pranayetta.1@gmail.com>
2024-04-06 16:58:47 -0700

    Roboshop shell script

--- catalogue.sh
+++ catalogue.sh
@@ -1,41 +1,44 @@
-echo -e "\e[32mDisabling the default Nodejs and enabling 18th version\e[0m"
-dnf module disable nodejs -y &>>/tmp/roboshop.log
-dnf module enable nodejs:18 -y &>>/tmp/roboshop.log
+source common.sh
+component=catalogue
 
-echo -e "\e[32mInstalling the nodejs\e[0m"
-dnf install nodejs -y &>>/tmp/roboshop.log
+echo -e "${color}Disabling the default Nodejs and enabling 18th version${nocolor}"
+dnf module disable nodejs -y &>>$log_file
+dnf module enable nodejs:18 -y &>>$log_file
 
-echo -e "\e[32mAdding the application user\e[0m"
-useradd roboshop &>>/tmp/roboshop.log
+echo -e "${color}Installing the nodejs${nocolor}"
+dnf install nodejs -y &>>$log_file
 
-echo -e "\e[32mCreating the application directory\e[0m"
-rm -rf /app &>>/tmp/roboshop.log
-mkdir /app &>>/tmp/roboshop.log
+echo -e "${color}Adding the application user${nocolor}"
+useradd roboshop &>>$log_file
 
-echo -e "\e[32mDownloading the application code and extracting\e[0m"
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>/tmp/roboshop.log
-cd /app &>>/tmp/roboshop.log
-unzip /tmp/catalogue.zip &>>/tmp/roboshop.log
+echo -e "${color}Creating the application directory${nocolor}"
+rm -rf $app_path &>>$log_file
+mkdir $app_path &>>$log_file
 
-echo -e "\e[32mInstalling the dependencies\e[0m"
-cd /app &>>/tmp/roboshop.log
-npm install &>>/tmp/roboshop.log
+echo -e "${color}Downloading the application code and extracting${nocolor}"
+curl -o /tmp/$component.zip https://roboshop-artifacts.s3.amazonaws.com/$component.zip &>>$log_file
+cd $app_path &>>$log_file
+unzip /tmp/$component.zip &>>$log_file
 
-echo -e "\e[32mCopying the catalogue service file\e[0m"
-cp /home/centos/Roboshop-shell/catalogue.service /etc/systemd/system/catalogue.service &>>/tmp/roboshop.log
+echo -e "${color}Installing the dependencies${nocolor}"
+cd $app_path &>>$log_file
+npm install &>>$log_file
 
-echo -e "\e[32mreloading\e[0m"
-systemctl daemon-reload &>>/tmp/roboshop.log
+echo -e "${color}Copying the $component service file${nocolor}"
+cp /home/centos/Roboshop-shell/$component.service /etc/systemd/system/$component.service &>>$log_file
 
-echo -e "\e[32menable and start\e[0m"
-systemctl enable catalogue &>>/tmp/roboshop.log
-systemctl restart catalogue &>>/tmp/roboshop.log
+echo -e "${color}reloading${nocolor}"
+systemctl daemon-reload &>>$log_file
 
-echo -e "\e[32mCopying mongodb repofile\e[0m"
-cp /home/centos/Roboshop-shell/mongodb.repo /etc/yum.repos.d/mongo.repo &>>/tmp/roboshop.log
+echo -e "${color}enable and start${nocolor}"
+systemctl enable $component &>>$log_file
+systemctl restart $component &>>$log_file
 
-echo -e "\e[32mInstall mongodb\e[0m"
-dnf install mongodb-org-shell -y &>>/tmp/roboshop.log
+echo -e "${color}Copying mongodb repofile${nocolor}"
+cp /home/centos/Roboshop-shell/mongodb.repo /etc/yum.repos.d/mongo.repo &>>$log_file
 
-echo -e "\e[32mLoad schema\e[0m"
-mongo --host mongodb-dev.pranaydevops73.me </app/schema/catalogue.js &>>/tmp/roboshop.log
+echo -e "${color}Install mongodb${nocolor}"
+dnf install mongodb-org-shell -y &>>$log_file
+
+echo -e "${color}Load schema${nocolor}"
+mongo --host mongodb-dev.pranaydevops73.me <$app_path/schema/$component.js &>>$log_file
