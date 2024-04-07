4d33e534ab1468d56e3cabec08a7d0858601f295
pranay <pranayetta.1@gmail.com>
2024-04-06 17:15:36 -0700

    Roboshop shell script

--- cart.sh
+++ cart.sh
@@ -1,32 +1,35 @@
-echo -e "\e[32mDisabling the default Nodejs and enabling 18th version\e[0m"
-dnf module disable nodejs -y &>>/tmp/roboshop.log
-dnf module enable nodejs:18 -y &>>/tmp/roboshop.log
+source common.sh
+component=cart
 
-echo -e "\e[32mInstalling the nodejs\e[0m"
-dnf install nodejs -y &>>/tmp/roboshop.log
+echo -e "${color}Disabling the default Nodejs and enabling 18th version${nocolor}"
+dnf module disable nodejs -y &>>${log_file}
+dnf module enable nodejs:18 -y &>>${log_file}
 
-echo -e "\e[32mAdding the application user\e[0m"
-useradd roboshop &>>/tmp/roboshop.log
+echo -e "${color}Installing the nodejs${nocolor}"
+dnf install nodejs -y &>>${log_file}
 
-echo -e "\e[32mCreating the application directory\e[0m"
-rm -rf /app &>>/tmp/roboshop.log
-mkdir /app &>>/tmp/roboshop.log
+echo -e "${color}Adding the application user${nocolor}"
+useradd roboshop &>>${log_file}
 
-echo -e "\e[32mDownloading the application code and extracting\e[0m"
-curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip &>>/tmp/roboshop.log
-cd /app &>>/tmp/roboshop.log
-unzip /tmp/cart.zip &>>/tmp/roboshop.log
+echo -e "${color}Creating the application directory${nocolor}"
+rm -rf ${app_path} &>>${log_file}
+mkdir ${app_path} &>>${log_file}
 
-echo -e "\e[32mInstalling the dependencies\e[0m"
-cd /app &>>/tmp/roboshop.log
-npm install &>>/tmp/roboshop.log
+echo -e "${color}Downloading the application code and extracting${nocolor}"
+curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log_file}
+cd ${app_path} &>>${log_file}
+unzip /tmp/${component}.zip &>>${log_file}
 
-echo -e "\e[32mCopying the cart service file\e[0m"
-cp /home/centos/Roboshop-shell/cart.service /etc/systemd/system/cart.service &>>/tmp/roboshop.log
+echo -e "${color}Installing the dependencies${nocolor}"
+cd ${app_path} &>>${log_file}
+npm install &>>${log_file}
 
-echo -e "\e[32mReloading\e[0m"
-systemctl daemon-reload &>>/tmp/roboshop.log
+echo -e "${color}Copying the ${component} service file${nocolor}"
+cp /home/centos/Roboshop-shell/${component}.service /etc/systemd/system/${component}.service &>>${log_file}
 
-echo -e "\e[32mEnable and start\e[0m"
-systemctl enable cart &>>/tmp/roboshop.log
-systemctl restart cart &>>/tmp/roboshop.log
+echo -e "${color}Reloading${nocolor}"
+systemctl daemon-reload &>>${log_file}
+
+echo -e "${color}Enable and start${nocolor}"
+systemctl enable ${component} &>>${log_file}
+systemctl restart ${component} &>>${log_file}
