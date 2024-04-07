1a15847c9e435276dc022f040613336e2c69c16e
pranay <pranayetta.1@gmail.com>
2024-04-06 17:30:43 -0700

    Roboshop shell script

--- catalogue.sh
+++ catalogue.sh
@@ -1,38 +1,7 @@
 source common.sh
 component=catalogue
 
-echo -e "${color}Disabling the default Nodejs and enabling 18th version${nocolor}"
-dnf module disable nodejs -y &>>$log_file
-dnf module enable nodejs:18 -y &>>$log_file
-
-echo -e "${color}Installing the nodejs${nocolor}"
-dnf install nodejs -y &>>$log_file
-
-echo -e "${color}Adding the application user${nocolor}"
-useradd roboshop &>>$log_file
-
-echo -e "${color}Creating the application directory${nocolor}"
-rm -rf $app_path &>>$log_file
-mkdir $app_path &>>$log_file
-
-echo -e "${color}Downloading the application code and extracting${nocolor}"
-curl -o /tmp/$component.zip https://roboshop-artifacts.s3.amazonaws.com/$component.zip &>>$log_file
-cd $app_path &>>$log_file
-unzip /tmp/$component.zip &>>$log_file
-
-echo -e "${color}Installing the dependencies${nocolor}"
-cd $app_path &>>$log_file
-npm install &>>$log_file
-
-echo -e "${color}Copying the $component service file${nocolor}"
-cp /home/centos/Roboshop-shell/$component.service /etc/systemd/system/$component.service &>>$log_file
-
-echo -e "${color}reloading${nocolor}"
-systemctl daemon-reload &>>$log_file
-
-echo -e "${color}enable and start${nocolor}"
-systemctl enable $component &>>$log_file
-systemctl restart $component &>>$log_file
+nodejs
 
 echo -e "${color}Copying mongodb repofile${nocolor}"
 cp /home/centos/Roboshop-shell/mongodb.repo /etc/yum.repos.d/mongo.repo &>>$log_file
--- common.sh
+++ common.sh
@@ -2,3 +2,38 @@ color="\e[32m"
 nocolor="\e[0m"
 log_file="/tmp/roboshop.log"
 app_path="/app"
+
+nodejs() {
+    echo -e "${color}Disabling the default Nodejs and enabling 18th version${nocolor}"
+    dnf module disable nodejs -y &>>$log_file
+    dnf module enable nodejs:18 -y &>>$log_file
+
+    echo -e "${color}Installing the nodejs${nocolor}"
+    dnf install nodejs -y &>>$log_file
+
+    echo -e "${color}Adding the application user${nocolor}"
+    useradd roboshop &>>$log_file
+
+    echo -e "${color}Creating the application directory${nocolor}"
+    rm -rf $app_path &>>$log_file
+    mkdir $app_path &>>$log_file
+
+    echo -e "${color}Downloading the application code and extracting${nocolor}"
+    curl -o /tmp/$component.zip https://roboshop-artifacts.s3.amazonaws.com/$component.zip &>>$log_file
+    cd $app_path &>>$log_file
+    unzip /tmp/$component.zip &>>$log_file
+
+    echo -e "${color}Installing the dependencies${nocolor}"
+    cd $app_path &>>$log_file
+    npm install &>>$log_file
+
+    echo -e "${color}Copying the $component service file${nocolor}"
+    cp /home/centos/Roboshop-shell/$component.service /etc/systemd/system/$component.service &>>$log_file
+
+    echo -e "${color}reloading${nocolor}"
+    systemctl daemon-reload &>>$log_file
+
+    echo -e "${color}enable and start${nocolor}"
+    systemctl enable $component &>>$log_file
+    systemctl restart $component &>>$log_file
+}
